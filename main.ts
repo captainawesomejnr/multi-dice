a3ed25146a6fee07c1d89efc31b6428f15124b39
Captain Awesome Jnr <90992828+captainawesomejnr@users.noreply.github.com>
2022-01-21 20:00:01 +1100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber > Dice) {
+        basic.showIcon(IconNames.Sad)
+        basic.pause(2000)
+        control.reset()
+    } else if (receivedNumber == Dice) {
+        basic.showIcon(IconNames.Asleep)
+    } else {
+        basic.showIcon(IconNames.Happy)
+        basic.pause(2000)
+        control.reset()
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    Dice = randint(1, 6)
+    basic.showNumber(Dice)
+    radio.sendNumber(Dice)
+})
+let Dice = 0
+radio.setGroup(202)
