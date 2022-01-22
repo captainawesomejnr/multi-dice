9f2fdc86f0be9214c37ec252d10679dd50c2bc7a
Captain Awesome Jnr <90992828+captainawesomejnr@users.noreply.github.com>
2022-01-22 15:48:51 +1100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -13,8 +13,22 @@ radio.onReceivedNumber(function (receivedNumber) {
     }
 })
 input.onGesture(Gesture.Shake, function () {
-    Dice = randint(1, 100)
-    basic.showNumber(Dice)
+    Dice = randint(1, 16)
+    if (Dice == 11) {
+        basic.showString("A")
+    } else if (Dice == 12) {
+        basic.showString("B")
+    } else if (Dice == 13) {
+        basic.showString("C")
+    } else if (Dice == 14) {
+        basic.showString("D")
+    } else if (Dice == 15) {
+        basic.showString("E")
+    } else if (Dice == 16) {
+        basic.showString("F")
+    } else {
+        basic.showNumber(Dice)
+    }
     radio.sendNumber(Dice)
 })
 let Dice = 0
