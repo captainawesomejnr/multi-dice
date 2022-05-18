e574ca0dfa6c11f3363bc7181abdcd5484406be9
Captain Awesome Jnr <90992828+captainawesomejnr@users.noreply.github.com>
2022-05-18 17:51:56 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -12,129 +12,131 @@ radio.onReceivedNumber(function (receivedNumber) {
         control.reset()
     }
 })
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . . . #
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        `)
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        `)
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . # . .
+        . . . . .
+        # . . . #
+        `)
+    basic.showLeds(`
+        # . # . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . # . #
+        `)
+    basic.showLeds(`
+        # . # . #
+        . . . . .
+        . . # . .
+        . . . . .
+        # . # . #
+        `)
+    basic.showLeds(`
+        # . # . #
+        . . . . .
+        # . . . #
+        . . . . .
+        # . # . #
+        `)
+    if (Dice == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (Dice == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # . . . #
+            . . . . .
+            . . . . .
+            `)
+    } else if (Dice == 3) {
+        basic.showLeds(`
+            . . # . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (Dice == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (Dice == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (Dice == 6) {
+        basic.showLeds(`
+            # . # . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . # . #
+            `)
+    } else if (Dice == 7) {
+        basic.showLeds(`
+            # . # . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . # . #
+            `)
+    } else {
+        basic.showLeds(`
+            # . # . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . # . #
+            `)
+    }
+    basic.clearScreen()
+})
 let Dice = 0
 radio.setGroup(202)
 Dice = randint(1, 8)
 radio.sendNumber(Dice)
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . # . .
-    . . . . .
-    . . . . .
-    `)
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    # . . . #
-    . . . . .
-    . . . . .
-    `)
-basic.showLeds(`
-    . . # . .
-    . . . . .
-    . . . . .
-    . . . . .
-    # . . . #
-    `)
-basic.showLeds(`
-    # . . . #
-    . . . . .
-    . . . . .
-    . . . . .
-    # . . . #
-    `)
-basic.showLeds(`
-    # . . . #
-    . . . . .
-    . . # . .
-    . . . . .
-    # . . . #
-    `)
-basic.showLeds(`
-    # . # . #
-    . . . . .
-    . . . . .
-    . . . . .
-    # . # . #
-    `)
-basic.showLeds(`
-    # . # . #
-    . . . . .
-    . . # . .
-    . . . . .
-    # . # . #
-    `)
-basic.showLeds(`
-    # . # . #
-    . . . . .
-    # . . . #
-    . . . . .
-    # . # . #
-    `)
-if (Dice == 1) {
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . # . .
-        . . . . .
-        . . . . .
-        `)
-} else if (Dice == 2) {
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        # . . . #
-        . . . . .
-        . . . . .
-        `)
-} else if (Dice == 3) {
-    basic.showLeds(`
-        . . # . .
-        . . . . .
-        . . . . .
-        . . . . .
-        # . . . #
-        `)
-} else if (Dice == 4) {
-    basic.showLeds(`
-        # . . . #
-        . . . . .
-        . . . . .
-        . . . . .
-        # . . . #
-        `)
-} else if (Dice == 5) {
-    basic.showLeds(`
-        # . . . #
-        . . . . .
-        . . # . .
-        . . . . .
-        # . . . #
-        `)
-} else if (Dice == 6) {
-    basic.showLeds(`
-        # . # . #
-        . . . . .
-        . . . . .
-        . . . . .
-        # . # . #
-        `)
-} else if (Dice == 7) {
-    basic.showLeds(`
-        # . # . #
-        . . . . .
-        . . # . .
-        . . . . .
-        # . # . #
-        `)
-} else {
-    basic.showLeds(`
-        # . # . #
-        . . . . .
-        # . . . #
-        . . . . .
-        # . # . #
-        `)
-}
-basic.clearScreen()
