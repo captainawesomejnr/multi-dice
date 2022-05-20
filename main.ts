31225d5b65cff132c870d7bb8c9f9618f24015c0
Captain Awesome Jnr <90992828+captainawesomejnr@users.noreply.github.com>
2022-05-21 09:39:19 +1000

    Restore '18/5/2022, 5:52:07 pm 5.1.5'

--- main.ts
+++ main.ts
@@ -2,23 +2,15 @@ radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber > Dice) {
         basic.showIcon(IconNames.Sad)
         basic.pause(2000)
-        music.playMelody("G B A G C5 B A B ", 120)
-        music.playTone(440, music.beat(BeatFraction.Double))
         control.reset()
     } else if (receivedNumber == Dice) {
         basic.showIcon(IconNames.Asleep)
-        music.playMelody("E B C5 A B G A F ", 120)
         control.reset()
     } else {
         basic.showIcon(IconNames.Happy)
         basic.pause(2000)
-        music.playMelody("C D E F G A B C5 ", 120)
         control.reset()
     }
-})
-input.onButtonPressed(Button.AB, function () {
-    basic.showString("EXITING GAME")
-    control.reset()
 })
 input.onGesture(Gesture.Shake, function () {
     basic.showLeds(`
@@ -77,9 +69,6 @@ input.onGesture(Gesture.Shake, function () {
         . . . . .
         # . # . #
         `)
-    radio.sendNumber(Dice)
-    basic.clearScreen()
-    basic.pause(500)
     if (Dice == 1) {
         basic.showLeds(`
             . . . . .
@@ -145,7 +134,9 @@ input.onGesture(Gesture.Shake, function () {
             # . # . #
             `)
     }
+    basic.clearScreen()
 })
 let Dice = 0
 radio.setGroup(202)
 Dice = randint(1, 8)
+radio.sendNumber(Dice)
